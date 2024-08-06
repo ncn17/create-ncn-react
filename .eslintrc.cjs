3f5ab2675c8cf6b742f27577143f3fc8f0b0a40c
ncn17 <nkounkounicephore@gmail.com>
2024-08-06 17:56:44 +0200

    config prettier with eslint for good code format

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,29 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'prettier'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    '@typescript-eslint/comma-dangle': 0,
+    'react/react-in-jsx-scope': 0,
+    'import/no-extraneous-dependencies': 0,
   },
-}
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json', './tsconfig.app.json'],
+    tsconfigRootDir: __dirname,
+  },
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-import React from "react";
-
-function App() {
-  return <h1>Hello world !</h1>;
+export default function App() {
+  return (
+    <>
+      <h1>Hello World !</h1>
+      <form>
+        <input
+          id="test"
+          name="test"
+          className="test-input"
+          aria-describedby="test_name_info"
+          placeholder="test input"
+        />
+      </form>
+    </>
+  );
 }
-
-export default App;
